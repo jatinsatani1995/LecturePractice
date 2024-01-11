98ea34beb3dc2a1b82534d8b4a719b347954e399
Jatin Satani <jatinsatani111@gmail.com>
2024-01-11 12:18:24 +0530

    Done

--- calciwithclss.py
+++ calciwithclss.py
@@ -1,4 +1,5 @@
 class calculator:
+
     def additon(self,a,b):
         return a+b
     
@@ -11,10 +12,11 @@ class calculator:
     def division(self,a,b):
         return a/b
 
-number1=int(input("Enter number1: "))
-number2=int(input("Enter number2: "))
+number1=int(input("Enter number1 : "))
+number2=int(input("Enter number2 : "))
 
 calci=calculator()
+
 print("addition is ",calci.additon(number1,number2))
 print("subtraction is ",calci.subtraction(number1,number2))
 print("multiplication is ",calci.multiplication(number1,number2))
--- msg.py
+++ msg.py
@@ -1,4 +1,11 @@
-x=50
-y =50
-sum = x+y
-print(sum)
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+
+x = Person("John", "Doe")
+x.printname()
